4dfcd4d8e2fd57f1791a441834435b56c16fec19
kennibc <kennibc@dearbornschools.org>
2017-01-13 11:39:18 -0500

    Fixed Core Renderer and Activity Edit Menu

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2017011200;
-$plugin->release  = 'v1.1.5';
+$plugin->version   = 2017011300;
+$plugin->release  = 'v1.1.6';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->requires  = 2016110800;
 $plugin->component = 'theme_fordson';
