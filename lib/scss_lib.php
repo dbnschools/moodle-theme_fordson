92d5568711aa581c10df7423d3de7f51a1f31aa1
kennibc <kennibc@dearbornschools.org>
2017-04-26 14:00:53 -0400

    version 1.4.3 fixed ada issue and logo issue

--- lib/scss_lib.php
+++ lib/scss_lib.php
@@ -139,7 +139,7 @@ function theme_fordson_get_pre_scss($theme) {
     $headerlogo = $theme->setting_file_url('headerlogo', 'headerlogo');
     if (isset($headerlogo)) {
         // Add a fade in transition to avoid the flicker on course headers ***.
-        $prescss .= '.headerlogo {background-image: url("'.$headerlogo.'"); background-repeat: no-repeat; background-position:left; height: 125px;}';
+        $prescss .= '.headerlogo {background-image: url("'.$headerlogo.'"); background-repeat: no-repeat; background-position:left;}';
     }
 
     // Set the default image for the header.
